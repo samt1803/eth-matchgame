794f425610f52c071042c545a8e8dbb4de626a8e
Sam <samt1803@gmail.com>
2018-08-26 18:54:27 +0200

    updated tests to work with different versions of ganache and testrpc

--- test/matchGameTests.js
+++ test/matchGameTests.js
@@ -11,8 +11,9 @@ before(() => contract('Matchgame', (_accounts) => {
 }));
 
 it('should be possible to create a new game', () => contractInstance.startNewGame(accounts[1])
-// assert that the transaction was successful
-  .then(result => assert.equal(result.receipt.status, '0x01', 'transaction to create account was not successful')),
+  // assert that the transaction was successful
+  // depending on the version 0x01 or 0x1 is returned as successful, 0x0 or 0x00 as failed
+  .then(result => assert.isTrue(result.receipt.status.endsWith('1'), 'transaction to create account was not successful')),
 );
 
 it('should be possible to get the game state. It should be the initial gamestate', () => contractInstance.getGame()
@@ -28,7 +29,7 @@ it('should be possible to get the game state. It should be the initial gamestate
 
 it('should be possible to make a move as player 2. Then the gamestate should have changed', () => contractInstance.makeMove(0, 1, { from: accounts[1] })
   // assert that the transaction was successful
-  .then(result => assert.equal(result.receipt.status, '0x01', 'transaction to create account was not successful'))
+  .then(result => assert.isTrue(result.receipt.status.endsWith('1'), 'transaction to create account was not successful'))
   // assert that the game state changed to the new state of 0,3,5,7 and that it is now player 1s turn
   .then(contractInstance.getGame().then((result) => {
     const state = result[0].map(e => e.c[0]);
@@ -79,7 +80,7 @@ it('owner should be able to pause the game. Then creating new game or moving is 
     .catch(err => assert.isTrue(err.toString().startsWith('Error: VM Exception while processing transaction: revert')))
     .then(() => contractInstance.pause({ from: accounts[0] }))
     // assert that the transaction was successful
-    .then(result => assert.equal(result.receipt.status, '0x01', 'transaction to create account was not successful'))
+    .then(result => assert.isTrue(result.receipt.status.endsWith('1'), 'transaction to create account was not successful'))
   // then starting new game or making a move should not work
     .then(() => contractInstance.startNewGame(accounts[1]))
     .catch(err => assert.isTrue(err.toString().startsWith('Error: VM Exception while processing transaction: revert'),
@@ -94,8 +95,8 @@ it('only the owner should be able to UNpause the game', () => contractInstance.u
   .catch(err => assert.isTrue(err.toString().startsWith('Error: VM Exception while processing transaction: revert'),
     'someone not the owner was able to unpause'))
   .then(() => contractInstance.unpause())
-  .then(result => assert.equal(result.receipt.status, '0x01', 'unpausing by the owner didn\'t work'))
+  .then(result => assert.isTrue(result.receipt.status.endsWith('1'), 'unpausing by the owner didn\'t work'))
   // it shoould no be possible to create a game again
   .then(() => contractInstance.startNewGame(accounts[1]))
-  .then(result => assert.equal(result.receipt.status, '0x01', 'creating game after unpausing didn\'t work')),
+  .then(result => assert.isTrue(result.receipt.status.endsWith('1'), 'creating game after unpausing didn\'t work')),
 );
